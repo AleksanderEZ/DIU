636c0210b43766f71e62578de73ade84f2107551
lucasolip <47455265+swavyfeel@users.noreply.github.com>
2021-09-28 19:59:23 +0100

    Initial RandomMatrixVisualizer commit

---
+++ RandomMatrixVisualizer/src/main/java/lucasandaleksander/randommatrixvisualizer/RandomMatrixVisualizer.java
@@ -0,0 +1,10 @@
+package lucasandaleksander.randommatrixvisualizer;
+
+public class RandomMatrixVisualizer {
+
+    public static void main(String[] args) {
+        Display window = new Display();
+        window.show();
+    }
+    
+}
