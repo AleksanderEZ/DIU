e9bb7f1499911f76b16727742ed92174939ccb5e
lucasolip <47455265+swavyfeel@users.noreply.github.com>
2021-10-12 22:06:50 +0100

    Finished

--- ComboPaint/src/main/java/view/Display.java
+++ ComboPaint/src/main/java/view/Display.java
@@ -1,6 +1,8 @@
 package view;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
+import java.awt.Color;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
@@ -8,29 +10,53 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
- * @author ALE-K
+ * @author ALE-K and Lucas
  */
 public class Display extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        backgroundLabel = new javax.swing.JLabel();
+        backgroundBox = new javax.swing.JComboBox<>();
         filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(100, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(32767, 0));
-        jComboBox2 = new javax.swing.JComboBox<>();
+        foregroundLabel = new javax.swing.JLabel();
+        foregroundBox = new javax.swing.JComboBox<>();
         canvas = new view.Canvas();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Paint!");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox1);
+        backgroundLabel.setText("Fondo");
+        jPanel1.add(backgroundLabel);
+
+        javax.swing.DefaultComboBoxModel<String> modelBackground = new javax.swing.DefaultComboBoxModel<>();
+        modelBackground.addAll(colorMap.keySet());
+        backgroundBox.setModel(modelBackground);
+        backgroundBox.setSelectedIndex(3);
+        backgroundBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backgroundBoxActionPerformed(evt);
+            }
+        });
+        jPanel1.add(backgroundBox);
 
         filler1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jPanel1.add(filler1);
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox2);
+        foregroundLabel.setText("Puntero");
+        jPanel1.add(foregroundLabel);
+
+        javax.swing.DefaultComboBoxModel<String> modelForeground = new javax.swing.DefaultComboBoxModel<>();
+        modelForeground.addAll(colorMap.keySet());
+        foregroundBox.setModel(modelForeground);
+        foregroundBox.setSelectedItem("Negro");
+        foregroundBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                foregroundBoxActionPerformed(evt);
+            }
+        });
+        jPanel1.add(foregroundBox);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.NORTH);
 
@@ -48,7 +74,7 @@ public class Display extends javax.swing.JFrame {
         );
         canvasLayout.setVerticalGroup(
             canvasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 270, Short.MAX_VALUE)
+            .addGap(0, 268, Short.MAX_VALUE)
         );
 
         getContentPane().add(canvas, java.awt.BorderLayout.CENTER);
@@ -56,20 +82,35 @@ public class Display extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    HashMap<String, Color> colorMap = new HashMap<>();
+    String[] colorNames = {"Blanco", "Negro", "Azul", "Rojo", "Verde", "Amarillo"};
+    Color[] colorValues = {Color.white, Color.black, Color.blue, Color.red, Color.green, Color.yellow};
+    
     private void canvasMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_canvasMouseMoved
         canvas.setMousePosition(evt.getX(), evt.getY());
         canvas.repaint();
     }//GEN-LAST:event_canvasMouseMoved
 
+    private void backgroundBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backgroundBoxActionPerformed
+        canvas.setBackgroundColor(colorMap.get(backgroundBox.getSelectedItem()));
+    }//GEN-LAST:event_backgroundBoxActionPerformed
+
+    private void foregroundBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_foregroundBoxActionPerformed
+        canvas.setForegroundColor(colorMap.get(foregroundBox.getSelectedItem()));
+    }//GEN-LAST:event_foregroundBoxActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> backgroundBox;
+    private javax.swing.JLabel backgroundLabel;
     private view.Canvas canvas;
     private javax.swing.Box.Filler filler1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JComboBox<String> foregroundBox;
+    private javax.swing.JLabel foregroundLabel;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
     
     public Display() {
+        initHashmap();
         setLookAndFeel();
         initComponents();
     }
@@ -79,6 +120,12 @@ public class Display extends javax.swing.JFrame {
             UIManager.setLookAndFeel( new FlatDarculaLaf() );
         } catch (UnsupportedLookAndFeelException ex) {
             Logger.getLogger(Display.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initHashmap() {
+        for (int i = 0; i < colorNames.length; i++) {
+            colorMap.put(colorNames[i], colorValues[i]);
         }
     }
 }
