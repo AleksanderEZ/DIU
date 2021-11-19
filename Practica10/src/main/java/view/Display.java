a71999aa589cac1f2f177ff7a643e1136398c4ce
lucas.olivares <lucasolivaresperez@gmail.com>
2021-11-19 18:41:41 +0000

    Added file add button functionality

--- Practica10/src/main/java/view/Display.java
+++ Practica10/src/main/java/view/Display.java
@@ -1,8 +1,15 @@
 package view;
 
+import java.io.File;
+import javax.swing.DefaultListModel;
+import javax.swing.UIManager;
+
 public class Display extends javax.swing.JFrame {
 
+    DefaultListModel<String> filesModel = new DefaultListModel<>();
+    
     public Display() {
+        setLookAndFeel();
         initComponents();
     }
     
@@ -14,55 +21,61 @@ public class Display extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel3 = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
+        zipSettingsPanel = new javax.swing.JPanel();
+        fileChooserPanel = new javax.swing.JPanel();
         fileChooser = new javax.swing.JFileChooser();
-        buttonChooserPanel = new javax.swing.JPanel();
+        chooserButtonsPanel = new javax.swing.JPanel();
         addButton = new javax.swing.JButton();
         removeButton = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
+        filesListPanel = new javax.swing.JScrollPane();
+        filesList = new javax.swing.JList<>();
         compressButtonPanel = new javax.swing.JPanel();
         compressButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Zipper");
+        setName("Zipper"); // NOI18N
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.PAGE_AXIS));
 
-        jPanel3.setLayout(new javax.swing.BoxLayout(jPanel3, javax.swing.BoxLayout.LINE_AXIS));
+        zipSettingsPanel.setLayout(new javax.swing.BoxLayout(zipSettingsPanel, javax.swing.BoxLayout.LINE_AXIS));
 
-        jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.LINE_AXIS));
-        jPanel1.add(fileChooser);
+        fileChooserPanel.setLayout(new javax.swing.BoxLayout(fileChooserPanel, javax.swing.BoxLayout.LINE_AXIS));
 
-        jPanel3.add(jPanel1);
+        fileChooser.setMultiSelectionEnabled(true);
+        fileChooser.setControlButtonsAreShown(false);
+        fileChooserPanel.add(fileChooser);
 
-        buttonChooserPanel.setLayout(new javax.swing.BoxLayout(buttonChooserPanel, javax.swing.BoxLayout.PAGE_AXIS));
+        zipSettingsPanel.add(fileChooserPanel);
+
+        chooserButtonsPanel.setLayout(new javax.swing.BoxLayout(chooserButtonsPanel, javax.swing.BoxLayout.PAGE_AXIS));
 
         addButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         addButton.setText("→");
-        buttonChooserPanel.add(addButton);
+        addButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addButtonActionPerformed(evt);
+            }
+        });
+        chooserButtonsPanel.add(addButton);
 
         removeButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         removeButton.setText("←");
-        buttonChooserPanel.add(removeButton);
+        chooserButtonsPanel.add(removeButton);
 
-        jPanel3.add(buttonChooserPanel);
+        zipSettingsPanel.add(chooserButtonsPanel);
 
-        jScrollPane1.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createEmptyBorder(15, 15, 15, 15), javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED)));
+        filesListPanel.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createEmptyBorder(15, 15, 15, 15), javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED)));
 
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Algo largo para probar la eficiencia", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(jList1);
+        filesList.setModel(filesModel);
+        filesListPanel.setViewportView(filesList);
 
-        jPanel3.add(jScrollPane1);
+        zipSettingsPanel.add(filesListPanel);
 
-        getContentPane().add(jPanel3);
+        getContentPane().add(zipSettingsPanel);
 
         compressButtonPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.RIGHT));
 
-        compressButton.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        compressButton.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         compressButton.setText("¡Comprimir!");
         compressButtonPanel.add(compressButton);
 
@@ -71,16 +84,38 @@ public class Display extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
+         File[] selectedFiles = fileChooser.getSelectedFiles();
+         for (File file : selectedFiles) {
+             String filename = file.getAbsolutePath();
+             if (!filesModel.contains(filename)) {
+                filesModel.addElement(filename); 
+             }
+         }
+         filesListPanel.revalidate();
+         filesListPanel.repaint();
+    }//GEN-LAST:event_addButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addButton;
-    private javax.swing.JPanel buttonChooserPanel;
+    private javax.swing.JPanel chooserButtonsPanel;
     private javax.swing.JButton compressButton;
     private javax.swing.JPanel compressButtonPanel;
     private javax.swing.JFileChooser fileChooser;
-    private javax.swing.JList<String> jList1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel fileChooserPanel;
+    private javax.swing.JList<String> filesList;
+    private javax.swing.JScrollPane filesListPanel;
     private javax.swing.JButton removeButton;
+    private javax.swing.JPanel zipSettingsPanel;
     // End of variables declaration//GEN-END:variables
+
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (Exception e) {
+           // handle exception
+        }
+    }
 }
